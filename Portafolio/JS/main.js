b39801097e0ce9858a99f6924db2667a81caaf18
drodriguezo18 <126374371+drodriguezo18@users.noreply.github.com>
2024-08-15 18:37:08 -0600

    finish css

--- Portafolio/JS/main.js
+++ Portafolio/JS/main.js
@@ -1,10 +1,16 @@
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
-
     sections.forEach(sec => {
         let top = window.screenY;
         let offset = sec.offsetTop - 100;
@@ -23,4 +29,7 @@ window.onscroll = () => {
     let header = document.querySelector('header')
 
     header.classList.toggle('sticky', window.scrollY > 100)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
